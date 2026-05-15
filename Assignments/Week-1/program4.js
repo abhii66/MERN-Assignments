1bcf12430a52cef8f8be4b0f116514001dcb1152
abhii66 <abhireddy0137@gmail.com>
2026-05-15 13:46:04 +0530

    Refactor min finding logic with comments and variable rename
    Updated variable name from 'min' to 'minimum' for clarity and added comments to explain the code.

--- Assignments/Week-1/program4.js
+++ Assignments/Week-1/program4.js
@@ -1,8 +1,10 @@
 // 4.find the min from the array 
 let marks=[90,78,65,98]
 let i=0
-let min=marks[i]
+let minimum=marks[i]
+//loop to iterate the array to find the minimum
 for(i=1;i<marks.length;i++)
-    if(marks[i]<min)
-        min=marks[i]
-console.log(`minimum element is ${min}.`)
+    //checks if the current element is minimum
+    if(marks[i]<minimum)
+        minimum=marks[i]
+console.log(`minimum element is ${minimum}.`)
